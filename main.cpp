2e8227090cdd0065441cfed82ae829f0be4641c7
chase cameron <chase cameron>
2017-02-23 20:54:13 -0800

    Changed align so that it works with int arrays. Also move align and other helper functions to main. (This should be cleaned up).

--- main.cpp
+++ main.cpp
@@ -3,60 +3,192 @@
 #include <vector>
 #include <fstream>
 
-#include "otherfuncs.h"
+//#include "otherfuncs.h"
 
 using namespace std;
 
+int* stringToIntArray(string dna);
+string intArrayToString(int* digitDNA);
+int align(int* inputOne, int* inputTwo, int inputOneSize, int inputTwoSize, int alpha[5][5], int* outputOne, int* outputTwo);
+int min(int a, int b, int c);
+
 int main() {
-    ifstream infile;
-    infile.open("imp2cost.txt");
-    if(!infile) {
-        cout << "The file imp2cost.txt does not exist." << endl;
-    }
+    int arr[5][5] = {   //	 -  A  T  G  C
+                            {0, 1, 2, 1, 3},//-
+                            {1, 0, 1, 5, 1},//A
+                            {2, 1, 0, 9, 1},//T
+                            {1, 5, 9, 0, 1},//G
+                            {3, 1, 1, 1, 0}	//C
+                    };
+    string a1 = "AACAGTTACC", b1 = "TAAGGTCA", a2, b2;
+	int *dnaOne, *dnaTwo;
+	int *pairOne, *pairTwo;
+	int pairSize = (a1.size() + b1.size());
+	int weight;
 
-    int arr[5][5] = {
-        {0, 1, 2, 1, 3},
-        {1, 0, 1, 5, 1},
-        {2, 1, 0, 9, 1},
-        {1, 5, 9, 0, 1},
-        {3, 1, 1, 1, 0}
-    };
+	pairOne = new int[pairSize];
+	pairTwo = new int[pairSize];
 
-    // The input strings that need to be aligned
-    string a1 = "AACAGTTACC";
-    string b1 = "TAAGGTCA";
+	for(int i = 0; i < pairSize; i++) {
+        pairOne[i] = 9;
+        pairTwo[i] = 9;
+	}
 
-    // Penalty for any matrix_size matched with a gap
-    int gap_penalty = 1;
+    //cout << "Data has been initialized." << endl;
 
-    /*
-     * alpha[i][j] = penalty for matching the ith matrix_size with the
-     *               jth matrix_size.
-     * Here: Penalty for matching an char with anoter one is 2
-     *       Penalty for matching an char with itself is 0
-     */
-    int alpha[matrix_size][matrix_size];
-    for (int i = 0; i < matrix_size; ++i) {
-        for (int j = 0; j < matrix_size; ++j) {
-            if (i == j) {
-                alpha[i][j] = 0;
-            }
-            else {
-                alpha[i][j] = 2;
-            }
+	dnaOne = stringToIntArray(a1);
+	dnaTwo = stringToIntArray(b1);
+
+    //cout << "Int arrays have been created." << endl;
+
+	weight = align(dnaOne, dnaTwo, a1.size(), b1.size(), arr, pairOne, pairTwo);
+
+    //cout << "Align has run successfully." << endl;
+
+    a2 = intArrayToString(pairOne);
+    b2 = intArrayToString(pairTwo);
+
+    cout << "1st String: " << a2 << endl;
+    cout << "2nd String: " << b2 << endl;
+    cout << "Current weight is: " << weight << endl;
+
+    // Aligned sequences
+    //string a2, b2;
+    //int penalty = align(a1, b1, arr, a2, b2);
+    return 0;
+}
+
+int* stringToIntArray(string dna) {
+    int stringSize = dna.size();
+	int* digitDNA;
+
+	digitDNA = new int[stringSize];
+
+	for(int i = 0; i < stringSize; ++i) {
+		if(dna[i] == 'A') {
+			digitDNA[i] = 1;
+		}
+
+		else if(dna[i] == 'T') {
+			digitDNA[i] = 2;
+		}
+
+		else if(dna[i] == 'G') {
+			digitDNA[i] = 3;
+		}
+
+		else if(dna[i] == 'C') {
+			digitDNA[i] = 4;
+		}
+	}
+
+	return digitDNA;
+}
+
+string intArrayToString(int* digitDNA) {
+    string dna;
+    for(int i = 0; digitDNA[i] != 9; i++) {
+        if(digitDNA[i] == 0) {
+            dna = dna + '-';
+        }
+        else if(digitDNA[i] == 1) {
+            dna = dna + 'A';
+        }
+        else if(digitDNA[i] == 2) {
+            dna = dna + 'T';
+        }
+        else if(digitDNA[i] == 3) {
+            dna = dna + 'G';
+        }
+        else if(digitDNA[i] == 4) {
+            dna = dna + 'C';
         }
     }
 
-    // Aligned sequences
-    string a2, b2;
-    int penalty = align(a1, b1, gap_penalty, alpha, a2, b2, arr);
+    return dna;
+}
 
-    cout << "Initial Sequence a: " << a1 << endl;
-    cout << "Initial Sequence b: " << b1 << endl;
-    cout << "Total Alignment Cost: " << penalty << endl;
-    cout << "Aligned sequences a & b: " << endl;
-    cout << a2 << endl;
-    cout << b2 << endl;
+int align(int* inputOne, int* inputTwo, int inputOneSize, int inputTwoSize, int alpha[5][5], int* outputOne, int* outputTwo) {
+    //cout << "We've made it inside the align function!" << endl;
+    int n = inputOneSize;
+    int m = inputTwoSize;
 
-    return 0;
+    vector<vector<int> > contour(n + 1, vector<int>(m + 1));
+    //int stringSize = dna.size();
+	//int* digitDNA;
+
+	//digitDNA = new int[stringSize];
+
+    //cout << "Contour vector was made successfully." << endl;
+
+    for (int i = 0; i <= m; i++) {
+        contour[0][i] = 3 * i;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        contour[i][0] = 3 * i;
+    }
+
+    //cout << "Contour was given some values here..." << endl;
+
+    //cout << "M: " << m << " | N: " << n << endl;
+    for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= m; j++) {
+			//cout << "Loop is about to run for the " << (i*j) << "th time" << endl;
+			contour[i][j] = min(	contour[i-1][j-1] + alpha[inputOne[i-1]][inputTwo[j-1]],
+                                    contour[i-1][j] + alpha[inputOne[i-1]][0],
+                                    contour[i][j-1] + alpha[0][inputTwo[j-1]]);
+            //cout << "Loop has run " << (i*j) << " times." << endl;
+		}
+	}
+
+	//cout << "Contour was initialized successfully!" << endl;
+
+    int j = m;
+    int i = n;
+    int x = 0;
+    int y = 0;
+    for (; i >= 1 && j >= 1; --i) {
+        if (contour[i-1][j-1] + alpha[inputOne[i-1]][inputTwo[j-1]])  {
+            outputOne[x] = inputOne[i-1];
+            outputTwo[y] = inputTwo[j-1];
+            --j; x++; y++;
+        }
+        else if (contour[i-1][j] + alpha[0][inputTwo[j-1]]) {
+            outputOne[x] = inputOne[i-1];
+            outputTwo[y] = 0;
+            x++; y++;
+        }
+        else {
+            outputOne[x] = 0;
+            outputTwo[y] = inputTwo[j-1];
+            --j;
+        }
+    }
+
+    while (i >= 1 && j < 1) {
+        outputOne[x] = inputOne[i-1];
+        outputTwo[y] = 0;
+        --i; x++; y++;
+    }
+    while (j >= 1 && i < 1) {
+        outputOne[x] = 0;
+        outputTwo[y] = inputTwo[j-1];
+        --j; x++; y++;
+    }
+
+    return contour[n][m];
+}
+
+int min(int a, int b, int c) {
+    //cout << "Min has been run." << endl;
+    if (a <= b && a <= c) {
+        return a;
+    }
+    else if (b <= a && b <= c) {
+        return b;
+    }
+    else {
+        return c;
+    }
 }
--- otherfuncs.cpp
+++ otherfuncs.cpp
@@ -1,31 +1,79 @@
 #include "otherfuncs.h"
 
-int align(const string &a, const string &b, int alpha_gap, int alpha[matrix_size][matrix_size], string &a_aligned, string &b_aligned,
-          int weights[matrix_size][matrix_size]) {
-    int n = a.size();
+int align(const string &a, const string &b, int alpha_gap,
+		  int alpha[matrix_size][matrix_size], string &a_aligned,
+		  string &b_aligned, int weights[matrix_size][matrix_size]) {
+
+	int n = a.size();
     int m = b.size();
 
+	//2D Vector of (n + 1) * (m + 1)
     vector<vector<int> > A(n + 1, vector<int>(m + 1));
 
+	
+	//Set the top edge to progressively higher values.
     for (int i = 0; i <= m; ++i) {
         A[0][i] = alpha_gap * i;
     }
+	
+	//Set the left edge to progressively higher values.
     for (int i = 0; i <= n; ++i) {
         A[i][0] = alpha_gap * i;
     }
+	
+	//This is done above so that we're sloped towards 0,0
 
+	//Rewritten to not use characters.
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= m; j++) {
+			A[i][j] = min(	A[i-1][j-1] + arr[x][y],	//
+							A[i-1][j] + arr[0][y],		//
+							A[i][j-1] + arr[x][0]);		//
+		}
+	}
+	
+		//  int arr[5][5] = {
+	//	 -  A  T  G  C	
+    //  {0, 1, 2, 1, 3},//-
+    //  {1, 0, 1, 5, 1},//A
+    //  {2, 1, 0, 9, 1},//T
+    //  {1, 5, 9, 0, 1},//G
+    //  {3, 1, 1, 1, 0}	//C
+    //};
+	
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
             char x_i = a[i-1]; //ith x coordinate
             char y_j = b[j-1]; //jth y coordinate
-            int gap_pen = gapWeight(weights, x_i, y_j);
-            cout << "The gap_pen for char " << x_i << " and " << y_j << " is: " << gap_pen << endl;
+            //int gap_pen = gapWeight(weights, x_i, y_j);
+            //cout << "The gap_pen for char " << x_i << " and " << y_j << " is: " << gap_pen << endl;
             A[i][j] = min(A[i-1][j-1] + alpha[x_i - 'A'][y_j - 'A'],
                           A[i-1][j] + alpha_gap,
                           A[i][j-1] + alpha_gap);
         }
     }
-
+	
+	//My version, without copying their spacing included...
+	int j = m;
+    int i = n;
+    for (; i >= 1 && j >= 1; --i) {
+        if (contour[i-1][j-1] + alpha[inputOne[i-1]][inputTwo[j-1]])  {
+            outputOne[] = inputOne[];
+            outputTwo[] = inputTwo[];
+            --j;
+        }
+        else if (contour[i-1][j] + alpha[0][inputTwo[j-1]]) {
+            outputOne[] = inputOne[];
+            outputTwo[] = 0;
+        }
+        else {
+            outputOne[] = 0;
+            outputTwo[] = inputTwo[];
+            --j;
+        }
+    }
+	
+	//Their version
     a_aligned = "";
     b_aligned = "";
     int j = m;
@@ -33,7 +81,7 @@ int align(const string &a, const string &b, int alpha_gap, int alpha[matrix_size
     for (; i >= 1 && j >= 1; --i) {
         char x_i = a[i-1];
         char y_j = b[j-1];
-        if (A[i][j] == A[i-1][j-1] + alpha[x_i - 'A'][y_j - 'A'])  {
+        if (A[i][j] == A[i-1][j-1] + /*alpha*/weights[x_i - 'A'][y_j - 'A'])  {
             a_aligned = x_i + a_aligned;
             b_aligned = y_j + b_aligned;
             --j;
@@ -86,6 +134,52 @@ void printVector(const vector<vector<int> > &A) {
 
 int gapWeight(int weights[matrix_size][matrix_size], char c1, char c2) {
     int return_weight = 0;
+	int positionOne = 0, positionTwo = 0;
+	
+	if(c1 == '-') {
+		positionOne = 0;
+	}
+	
+	else if(c1 == 'A') {
+		positionOne = 1;
+	}
+	
+	else if(c1 == 'T') {
+		positionOne = 2;
+	}	
+	
+	else if(c1 == 'G') {
+		positionOne = 3;
+	}
+	
+	else if(c1 == 'C') {
+		positionOne = 4;
+	}
+
+	
+	if(c2 == '-') {
+		positionTwo = 0;
+	}
+	
+	else if(c2 == 'A') {
+		positionTwo = 1;
+	}
+	
+	else if(c2 == 'T') {
+		positionTwo = 2;
+	}	
+	
+	else if(c1 == 'G') {
+		positionTwo = 3;
+	}
+	
+	else if(c1 == 'C') {
+		positionTwo = 4;
+	}
+	
+	return_weight = weights[positionOne][positionTwo];
+	
+	/*
 
     if (c1 == '-' && c2 == 'A') {
         return_weight = weights[0][1];
@@ -99,6 +193,8 @@ int gapWeight(int weights[matrix_size][matrix_size], char c1, char c2) {
     else if (c1 == '-' && c2 == 'C') {
         return_weight = weights[0][4];
     }
+	
+	
     else if (c1 == 'A' && c2 == '-') {
         return_weight = weights[1][0];
     }
@@ -111,6 +207,44 @@ int gapWeight(int weights[matrix_size][matrix_size], char c1, char c2) {
     else if (c1 == 'C' && c2 == '-') {
         return_weight = weights[4][0];
     }
+	
+	else if (c1 == '-' && c2 == '-') {
+        return_weight = weights[0][0];
+    }
+	
+	else if (c1 == 'A' && c2 == 'T') {
+        return_weight = weights[2][1];
+    }
+	else if (c1 == 'A' && c2 == 'G') {
+        return_weight = weights[3][1];
+    }
+	else if (c1 == 'A' && c2 == 'C') {
+        return_weight = weights[4][1];
+    }
+	
+	else if (c1 == 'T' && c2 == 'A') {
+        return_weight = weights[1][2];
+    }
+	else if (c1 == 'G' && c2 == 'A') {
+        return_weight = weights[1][3];
+    }
+	else if (c1 == 'C' && c2 == 'A') {
+        return_weight = weights[1][4];
+    }
+	
+	else if (c1 == 'A' && c2 == 'A') {
+        return_weight = weights[1][1];
+    }
+	
+	*/
+	
+	//	 -  A  T  G  C	
+	//	{0, 1, 2, 1, 3},	//-
+    //	{1, 0, 1, 5, 1},	//A
+    //	{2, 1, 0, 9, 1},	//T
+    //	{1, 5, 9, 0, 1},	//G
+	//  {3, 1, 1, 1, 0}		//C
+    //};
 
     return return_weight;
 }
